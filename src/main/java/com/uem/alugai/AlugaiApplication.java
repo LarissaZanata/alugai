787ac2c164a9aa817d9ec1419293550395d626e4
LarissaZanata <zanatalarissamorais@gmail.com>
2019-04-20 16:50:51 -0300

    Subindo projeto base com dependencias do spring e bancos mysql e derbi

---
+++ src/main/java/com/uem/alugai/AlugaiApplication.java
@@ -0,0 +1,13 @@
+package com.uem.alugai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugaiApplication.class, args);
+	}
+
+}
