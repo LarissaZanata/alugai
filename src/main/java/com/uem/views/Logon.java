f5565646220b9bf4bb9876786589fb95a283bd27
LarissaZanata <zanatalarissamorais@gmail.com>
2019-04-20 21:50:38 -0300

    View de login

--- src/main/java/com/uem/views/Logon.java
+++ src/main/java/com/uem/views/Logon.java
@@ -3,10 +3,16 @@ package com.uem.views;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.JButton;
 
 public class Logon {
 
-	private JFrame frame;
+	private JFrame frmAluga;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -16,7 +22,7 @@ public class Logon {
 			public void run() {
 				try {
 					Logon window = new Logon();
-					window.frame.setVisible(true);
+					window.frmAluga.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +41,36 @@ public class Logon {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmAluga = new JFrame();
+		frmAluga.setTitle("Alugaí 1.0");
+		frmAluga.setBounds(100, 100, 450, 300);
+		frmAluga.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmAluga.getContentPane().setLayout(null);
+		
+		JLabel lblLogin = new JLabel("Login");
+		lblLogin.setFont(new Font("Comic Sans MS", Font.BOLD, 12));
+		lblLogin.setBounds(81, 71, 46, 14);
+		frmAluga.getContentPane().add(lblLogin);
+		
+		JLabel lblSenha = new JLabel("Senha");
+		lblSenha.setFont(new Font("Comic Sans MS", Font.BOLD, 12));
+		lblSenha.setBounds(81, 121, 46, 14);
+		frmAluga.getContentPane().add(lblSenha);
+		
+		textField = new JTextField();
+		textField.setBounds(137, 68, 219, 20);
+		frmAluga.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(137, 118, 219, 20);
+		frmAluga.getContentPane().add(textField_1);
+		
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.setFont(new Font("Comic Sans MS", Font.BOLD, 12));
+		btnEntrar.setBounds(285, 177, 71, 23);
+		frmAluga.getContentPane().add(btnEntrar);
 	}
 
 }
